2c5bc7befa94dedd14414c0a01259647d8acdeb4
nbvanting <nicolai@vanting.com>
2025-06-11 19:43:42 +0200

    added verbosity

--- main.py
+++ main.py
@@ -7,30 +7,38 @@ from google import genai
 from google.genai import types
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
+def main():
+	load_dotenv()
+
+	args = sys.argv[1:]
+
+	if not args:
+		print("No prompt given in the CLI. Please provide a prompt.\nUsage: python main.py <prompt here>")
+		sys.exit(1)
+
+
+	api_key = os.environ.get("GEMINI_API_KEY")
+	client = genai.Client(api_key=api_key)
+	model = "gemini-2.0-flash-001"
+	user_prompt = args[0]
+
+	messages = [
+		types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+	]
+
+	response = client.models.generate_content(
+	    model=model, contents=messages
+	)
+	print(response.text)
+
+	if "--verbose" in args:
+		prompt_token_count = response.usage_metadata.prompt_token_count
+		candidates_token_count = response.usage_metadata.candidates_token_count
+		print("User prompt:", user_prompt)
+		print("Prompt tokens:", prompt_token_count)
+		print("Response tokens:", candidates_token_count)
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		client = genai.Client(api_key=api_key)
-		model = "gemini-2.0-flash-001"
-		user_prompt = sys.argv[1]
-
-		messages = [
-			types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-		]
-
-		response = client.models.generate_content(
-		    model=model, contents=messages
-		)
-		print(response.text)
-
-		prompt_token_count = response.usage_metadata.prompt_token_count
-		candidates_token_count = response.usage_metadata.candidates_token_count
-		print("Prompt tokens:", prompt_token_count)
-		print("Response tokens:", candidates_token_count)
-	else:
-		print("No prompt given in the CLI. Please provide a prompt. Example: python main.py <prompt here>")
-		exit(1)
+	main()
