0e54eb293191dccadf2204df7f06d3bdc4bf3230
nbvanting <nicolai@vanting.com>
2025-06-11 19:31:13 +0200

    added foundations for user conversation history

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
 import sys
+
 from dotenv import load_dotenv
+
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
+
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		client = genai.Client(api_key=api_key)
 		model = "gemini-2.0-flash-001"
-		contents = sys.argv[1]
+		user_prompt = sys.argv[1]
+
+		messages = [
+			types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+		]
 
 		response = client.models.generate_content(
-		    model=model, contents=contents
+		    model=model, contents=messages
 		)
 		print(response.text)
 
