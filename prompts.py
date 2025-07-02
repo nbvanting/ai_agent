192d136aebf5a1417e529468626968550ec03cec
nbvanting <nicolai@vanting.com>
2025-07-02 10:48:42 +0200

    final test to see if the agent can fix a bug in the code succesfully

--- prompts.py
+++ prompts.py
@@ -9,4 +9,10 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+When asked to fix bugs in code, you can:
+- Read files to understand the code
+- Execute code to test it
+- Write new or modified code to files
+- List files to find relevant code
 """
